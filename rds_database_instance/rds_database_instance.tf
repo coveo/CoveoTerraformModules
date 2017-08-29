fa209e39259a3977f76b6a226881a9db2e81f5af
jmorissette <julien_morissette@outlook.com>
2017-08-29 13:26:17 -0400

    Readd tags that where deleted by error

--- rds_database_instance/rds_database_instance.tf
+++ rds_database_instance/rds_database_instance.tf
@@ -7,6 +7,7 @@ resource "aws_db_subnet_group" "db_subnet_group" {
 
   name        = "${lookup(var.optional_parameters, "db_subnet_group_name", "${var.custom_identifier}")}"
   description = "${lookup(var.optional_parameters, "subnet_group_description", "${var.custom_identifier}")}"
+  tags        = "${var.subnet_group_tags}"
 }
 
 resource "aws_ssm_parameter" "db_root_username" {
