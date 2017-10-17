a7b4b54f81a2f328c9667b5f2d3bc6324f4d10e7
jmorissette <julien_morissette@outlook.com>
2017-10-17 16:31:29 -0400

    switch username ssm parametre from secureString to only string

--- rds_database_cluster/rds_database_cluster.tf
+++ rds_database_cluster/rds_database_cluster.tf
@@ -12,10 +12,8 @@ resource "aws_db_subnet_group" "db_subnet_group" {
 
 resource "aws_ssm_parameter" "db_root_master_username" {
   name  = "${lookup(var.optional_parameters, "parameter_store_path", "${var.custom_identifier}")}/Username"
-  type  = "SecureString"
+  type  = "String"
   value = "${lookup(var.optional_parameters, "master_username", "root")}"
-
-  key_id = "${lookup(var.optional_parameters, "master_username_kms_key_id", "")}"
 }
 
 resource "aws_ssm_parameter" "db_root_master_password" {
