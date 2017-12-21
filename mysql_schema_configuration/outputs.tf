3dad33eb2c4eb978a3afc9f8823cfe0e60b40340
Yannis Rivard-Mulrooney <yrivardmulrooney@coveo.com>
2017-12-21 14:48:26 -0500

    Add output for user/password param names

--- mysql_schema_configuration/outputs.tf
+++ mysql_schema_configuration/outputs.tf
@@ -2,7 +2,7 @@
  * Copyright (c) 2011 - 2017, Coveo Solutions Inc.
  */
 
-output "name" {
+output "user" {
   value = "${mysql_user.user.user}"
 }
 
@@ -12,4 +12,12 @@ output "host" {
 
 output "schema_name" {
   value = "${mysql_database.schema.name}"
+}
+
+output "user_param_name" {
+  value = "${aws_ssm_parameter.username.name}"
+}
+
+output "password_param_name" {
+  value = "${aws_ssm_parameter.password.name}"
 }
