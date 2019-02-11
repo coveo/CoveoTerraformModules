494815e70060b308b138ce3c309778cb91814ff1
Martin Laporte <mlaporte@coveo.com>
2019-02-11 15:17:25 +0100

    Add very basic module to create a feature flag in LaunchDarkly with the required properties for a critical update.

---
+++ critical-update/feature-flag.tf
@@ -0,0 +1,19 @@
+data "launchdarkly_project" "critical-updates" {
+  key = "critical-updates-${var.env}"
+}
+
+resource "launchdarkly_feature_flag" "remove-static-queries" {
+  project_key = "${data.launchdarkly_project.critical-updates.key}"
+  key = "${var.key}"
+  name = "${var.name}"
+  description = "${var.description}"
+  custom_properties = [{
+    key = "activation-date"
+    name = "Activation Date"
+    value = ["${var.activation_date}"]
+  }, {
+    key = "online-help-url"
+    name = "Online Help URL"
+    value = ["${var.online_help_url}"]
+  }]
+}
---
+++ critical-update/variables.tf
@@ -0,0 +1,19 @@
+variable "key" {
+  description = "The unique key for the critical update."
+}
+
+variable "name" {
+  description = "The name for the critical update (visible to customers)."
+}
+
+variable "description" {
+  description = "Short description of the critical update (visible to customers)."
+}
+
+variable "activation_date" {
+  description = "The date at which we will automatically activate the critical update (YYYY-MM-DD)"
+}
+
+variable "online_help_url" {
+  description = "The URL for the online help topic describing the critical update."
+}
