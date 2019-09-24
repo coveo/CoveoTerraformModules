ad9685a2ece65941ce606e1950b6b1c2e0026adf
Thomas <tdegiacinto@coveo.com>
2019-09-24 11:55:23 +0200

    Remove module for the moment

--- coveo-eks/modules/metrics_server/_variables.tf
+++
@@ -1,9 +0,0 @@
-variable "namespace" {
-  default     = "kube-system"
-  description = "Define in which namespace metrics_server will be deployed"
-}
-
-variable "helm_deployed" {
-  default     = false
-  description = "ensure dependency with Tiller"
-}
--- coveo-eks/modules/thanos/_locals.tf
+++
@@ -1,6 +0,0 @@
-locals {
-  # FQDN of Thanos SRV registry
-  srv_records = "_thanosstores._tcp.${var.domain_name}."
-
-  bucket_name = "coveo-${var.env}-thanos"
-}
