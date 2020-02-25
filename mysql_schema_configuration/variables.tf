77217f35bfb284373d68981e330adefb8ba0d1cb
Jean-Michel Provencher <jmprovencher@coveo.com>
2020-02-25 09:27:03 -0500

    adding kms key id

--- mysql_schema_configuration/variables.tf
+++ mysql_schema_configuration/variables.tf
@@ -6,6 +6,10 @@ variable "parameter_store_path" {}
 
 variable "schema_name" {}
 
+variable "password_kms_key_id" {
+  default = ""
+}
+
 variable "optional_parameters" {
   type        = "map"
   description = "Parameters with default values. Possible keys are username_aws_ssm_parameter_type and username_kms_key_id; if no key is present default value is used."
