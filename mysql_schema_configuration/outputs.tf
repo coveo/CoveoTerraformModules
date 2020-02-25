37c7a94fb80358dc3ef728432ef706fc529eabfd
Jean-Michel Provencher <52545119+jmprovencher-coveo@users.noreply.github.com>
2020-02-25 09:29:11 -0500

    Remove user creation from module (#32)
    * adding tags support for root username password
    
    * adding tag support for rds instance
    
    * adding ignore changes to modules
    
    * adding ignore changes parameter
    
    * adding ignore changes value to ssm parameter
    
    * Now ignoring all changes to password values for the mysql module
    
    * Remove user creation from mysql_schema_configuration module
    
    * remove empty line from README
    
    * fixing typo
    
    * adding kms key id

--- mysql_schema_configuration/outputs.tf
+++ mysql_schema_configuration/outputs.tf
@@ -2,14 +2,6 @@
  * Copyright (c) 2011 - 2019, Coveo Solutions Inc.
  */
 
-output "user" {
-  value = "${mysql_user.user.user}"
-}
-
-output "host" {
-  value = "${mysql_user.user.host}"
-}
-
 output "schema_name" {
   value = "${mysql_database.schema.name}"
 }
