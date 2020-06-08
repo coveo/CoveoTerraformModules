0f630cfabe66868701fdd5464163dae9550bf98e
mcoulombe <mcoulombe@coveo.com>
2020-06-08 18:31:19 -0400

    - removed outputs so I can clean up

--- rds_database_cluster/outputs.tf
+++ rds_database_cluster/outputs.tf
@@ -1,25 +1,3 @@
 /**
  * Copyright (c) 2011 - 2019, Coveo Solutions Inc.
  */
-
-output "endpoint" {
-  value = "${aws_rds_cluster.rds_db_cluster.endpoint}"
-}
-
-output "username" {
-  sensitive = true
-  value     = "${aws_ssm_parameter.db_root_master_username.value}"
-}
-
-output "password" {
-  sensitive = true
-  value     = "${aws_ssm_parameter.db_root_master_password.value}"
-}
-
-output "port" {
-  value = "${aws_rds_cluster.rds_db_cluster.port}"
-}
-
-output "cluster_arn" {
-  value = "${aws_rds_cluster.rds_db_cluster.arn}"
-}
